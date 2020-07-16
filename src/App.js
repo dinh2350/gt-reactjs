d68ce6f78488caff3573f26d2450fcc4b1beecab
dinh2350 <hao9x0159@gmail.com>
2020-07-16 21:22:17 +0700

    buoi3_prop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
-import BaiTapLayout2 from './Components/BaiTapLayout2/BaiTapLayout2';
-import DataBinding from './DataBinding/DataBinding';
-import EventBinding from './EventBinding/EventBinding';
-import RenderWithState from './RenderWithState/RenderWithState';
-import BaiTapChonXe from './RenderWithState/BaiTapChonXe';
-import RenderWithMap from './RenderWithState/RenderWithMap/RenderWithMap';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import BaiTapLayout from "./Components/BaiTapLayout/BaiTapLayout";
+import BaiTapLayout2 from "./Components/BaiTapLayout2/BaiTapLayout2";
+import DataBinding from "./DataBinding/DataBinding";
+import EventBinding from "./EventBinding/EventBinding";
+import RenderWithState from "./RenderWithState/RenderWithState";
+import BaiTapChonXe from "./RenderWithState/BaiTapChonXe";
+import RenderWithMap from "./RenderWithState/RenderWithMap/RenderWithMap";
+import ExampleGlasses from "./RenderWithState/ExampleGlasses";
+import DemoProps from "./Props/DemoProps";
 function App() {
   return (
     <div className="App">
-        {/* <BaiTapLayout /> */}
-        {/* <BaiTapLayout2 /> */}
-        {/* <DataBinding /> */}
-        {/* <EventBinding /> */}
-        {/* <RenderWithState /> */}
-        {/* <BaiTapChonXe /> */}
-        <RenderWithMap />
+      <BaiTapLayout />
+      {/* <BaiTapLayout2 /> */}
+      {/* <DataBinding /> */}
+      {/* <EventBinding /> */}
+      {/* <RenderWithState /> */}
+      {/* <BaiTapChonXe /> */}
+      {/* <RenderWithMap /> */}
+      {/* <ExampleGlasses /> */}
+      <DemoProps />
     </div>
   );
 }
